5bdd2a143dc40cfa05208bfb7d670ca44fe1367d
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-04-11 13:07:46 +0200

    aggiunte correzioni + nuovi metodi

--- main.js
+++ main.js
@@ -5,11 +5,69 @@ const student2 = new Student(`Luis Alberto`, `Castro`, 1970, 4, 6);
 const student3 = new Student(`Davide`, `Consigliere`, 1970, 0, 1);
 const student4 = new Student(`Chiara`, `Badile`, 1970, 4, 6);
 
-const classroom1 = new Classroom([student1, student2, student3, student4]);
 
-displayClassroom(classroom1.students);
+arrayOfStudents = [student1, student2, student3, student4]
+const classroom1 = new Classroom(arrayOfStudents);
 
-function displayClassroom(array) {
+displayClassroom2(classroom1);
+
+
+// CORREZIONE medodo 1
+function displayClassroom(classroom) {
+
+    const studentList = document.getElementById(`student-list`);
+
+    studentList.innerHTML = ``;
+
+    for (let i = 0; i < classroom.students.length; i++) {
+
+        const student = classroom.students[i];
+
+        const newLi = document.createElement(`li`);
+
+        const studentNode = document.createTextNode(student.surname + ` ` + student.name);
+
+        newLi.appendChild(studentNode);
+
+        const deleteBtn = document.createElement(`button`);
+
+        deleteBtn.classList.add(`remove-btn`);
+
+        deleteBtn.style.border = `solid lime 2px`;
+
+        // const btnNode = document.createTextNode(`Rimuovi`);
+
+        const feeshImg = document.createElement('img');
+
+        feeshImg.classList.add('remove-img')
+        
+        feeshImg.src = './assets/4x-1.gif';
+
+        deleteBtn.addEventListener(`click`, (event) => removeStudentFromClassroom(student));
+
+        deleteBtn.appendChild(feeshImg);
+
+        newLi.appendChild(deleteBtn);
+
+        studentList.appendChild(newLi);
+    }
+    
+}
+
+// CORREZIONE metodo 2, metodo short, scrivo direttamente l'HTML per aggiungere i miei li
+function displayClassroom2(classroom){
+    const studentList = document.getElementById(`student-list`);
+
+    studentList.innerHTML = ``;
+
+    for (let i = 0; i < classroom.students.length; i++) {
+        const student = classroom.students[i];
+        studentList.innerHTML += `<li class="list-element">${student.name} ${student.surname}<button> Rimuovi </button></li>`;
+    }
+}
+
+// FATTO A CASA
+/*function displayClassroom(array) {
     document.getElementById(`student-list`).innerHTML = ``;
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
@@ -60,17 +118,38 @@ function displayClassroom(array) {
         newli.appendChild(removeButton); // appendo il bottone al <li>
         mainUL.appendChild(newli); // Appendo il <li> alla lista
         */
-    }
-}
+    /*}
+}*/
 
-
-
+// CORREZIONE
 function shuffleTheClassroom() {
     classroom1.shuffleStudents();
-    displayClassroom(classroom1.students);
+    displayClassroom(classroom1);
 }
 
+// FATTO IN CASA
+// function shuffleTheClassroom() {
+//     classroom1.shuffleStudents();
+//     displayClassroom(classroom1.students);
+// }
+
+//CORREZIONE 
 function addStudentToClassroom() {
+    const nameInput = document.getElementById(`student-name`);
+    const surnameInput = document.getElementById(`student-surname`);
+
+    const name = nameInput.value;
+    const surname = surnameInput.value;
+
+    if (name !== `` && surname !== ``){   
+        const newStudent = new Student (name,surname);
+        classroom1.addStudent(newStudent);
+        displayClassroom(classroom1);
+    }    
+}
+
+// FATTO A CASA
+/*function addStudentToClassroom() {
     const input1 = document.getElementById(`student-name`);
     const input2 = document.getElementById(`student-surname`);
     const input3 = document.getElementById(`student-moB`);
@@ -78,33 +157,19 @@ function addStudentToClassroom() {
     const input5 = document.getElementById(`student-yoB`);
     if (input1.value !== `` && input2.value !== ``) // Se casella di testa vuota, non aggiungere nulla!
     {
-        const newStudentName = input1.value; // creo una stringa qualsiasi da aggiungere al mio array 
-        const newStudentSurname = input2.value; // creo una stringa qualsiasi da aggiungere al mio array 
+        const newStudentName = input1.value; 
+        const newStudentSurname = input2.value;
         const newStudentMoB = input3.value;
         const newStudentDoB = input4.value;
         const newStudentYoB = input5.value;
-        /*if (isNaN(newStudentMoB) !== true && isNaN(newStudentDoB) !== true && isNaN(newStudentYoB) !== true) {
-            // questo mi da solo una cosa che voglio io, non l'utente, aggiungo input per testo
-            const aNewStudent = new Student(input1.value, input2.value, Number(input5.value), Number(input3.value), Number(input4.value));
-            console.log(aNewStudent);
-            classroom1.addStudent(aNewStudent); // pusho la stringa
-            displayClassroom(classroom1.students); // rimostro la lista, ma questo vuol dire che mi stampa tutti gli elementi compresi quelli precedenti, 
-            // mi serve svuotare la lista prima! Aggiungo linea 40
-            input1.value = ``;
-            input2.value = ``;
-            input3.value = ``;
-            input4.value = ``;
-            input5.value = ``;*/
 
         if (Number(input4.value) <= 31 && Number(input4.value) >= 0) {
             if (Number(input3.value) <= 12 && Number(input3.value) >= 0) {
                 if (isNaN(newStudentMoB) !== true && isNaN(newStudentDoB) !== true && isNaN(newStudentYoB) !== true) {
-                    // questo mi da solo una cosa che voglio io, non l'utente, aggiungo input per testo
                     const aNewStudent = new Student(input1.value, input2.value, Number(input5.value), Number(input3.value), Number(input4.value));
                     console.log(aNewStudent);
-                    classroom1.addStudent(aNewStudent); // pusho la stringa
-                    displayClassroom(classroom1.students); // rimostro la lista, ma questo vuol dire che mi stampa tutti gli elementi compresi quelli precedenti, 
-                    // mi serve svuotare la lista prima! Aggiungo linea 40
+                    classroom1.addStudent(aNewStudent);
+                    displayClassroom(classroom1.students);
                     input1.value = ``;
                     input2.value = ``;
                     input3.value = ``;
@@ -114,8 +179,21 @@ function addStudentToClassroom() {
             }
         }
     }
+}*/
+
+function removeStudentFromClassroom(student){
+    classroom1.removeStudent(student);
+    displayClassroom(classroom1);
 }
 
+function colorMainTitle(){
+    const mainTitle = document.querySelector('.main-title'); // prende il primo match come in css (., "#", "h1", "#student-list li")
+    // se invece di prendere il primo voglio li prenda tutti uso querySelectorAll
+    mainTitle.style.color = 'tomato';
+}
+
+
+// JUNK
 // function removeStudentToClassroom(element){
 //     classroom1.removeStudent(classroom1.students);
 //     displayClassroom(classroom1.students);
